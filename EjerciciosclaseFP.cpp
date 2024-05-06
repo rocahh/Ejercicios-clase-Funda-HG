f985c945cc1e0aa5961220df213fad0f363cc216
rocahh <164081392+rocahh@users.noreply.github.com>
2024-05-06 12:39:42 -0600

    agregando 2 ejercicios de FP

---
+++ EjerciciosclaseFP.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 1;
+    while (i<13)
+    {
+        cout << "12" << i << 12*1 << "\n";
+        i++;
+    }
+    return 0;
+    
+}
---
+++ claseFP.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 1;
+    while (1<10)
+    {
+        cout << i;
+        i++;
+    }
+    return 0;
+}
